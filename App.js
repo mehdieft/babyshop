9c21abcac9933a8b747e0acd293f3b514fa52414
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-07-10 21:13:42 +0430

    design task with react native

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button,Image} from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View>
+    
+  </View>
+   
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    marginLeft:10,
+    marginRight:10,
+    marginTop:100,
   },
+  childIcon:{
+    borderRadius: 50,
+  },
+  image: {
+    width: 107,
+    height: 165,
+    padding: 10
+  }
+ 
 });
