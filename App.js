a4e01c51b77ed36a5388c7d66833c06cbb594c34
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-07-11 02:56:40 +0430

    finish first page

--- App.js
+++ App.js
@@ -1,29 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Button,Image} from 'react-native';
-
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { useWindowDimensions } from 'react-native';
 
 export default function App() {
+  const window = useWindowDimensions();
   return (
-  <View>
-    
-  </View>
-   
+
+    <View>
+      <View style={styles.container}>
+        <View style={styles.babyIconInner}>
+          <Image style={styles.babyIcon} source={require('./assets/baby.png')} />
+        </View>
+        <Text style={styles.textTitle}>wellcome to Growly</Text>
+        <Text style={styles.textField}>Watch your baby grow, log your symptoms and learn what to expect week by week with Growly Pregnancy!</Text>
+      </View>
+      <Image style={styles.vector} source={require('./assets/Vector.png')} />
+
+    </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    marginLeft:10,
-    marginRight:10,
-    marginTop:100,
+  container: {
+    marginTop: 100,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 18,
+
+
   },
-  childIcon:{
-    borderRadius: 50,
+  textTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#25396F',
+    marginBottom: 8,
   },
-  image: {
-    width: 107,
-    height: 165,
-    padding: 10
-  }
- 
+  babyIconInner: {
+    backgroundColor: '#3EB09B',
+    borderRadius: 80,
+    height: 156,
+    width: 156,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 18
+
+  },
+  babyIcon: {
+    width: 92,
+    height: 92,
+  },
+  textField: {
+    textAlign: 'center',
+    maxWidth: '50%',
+    fontSize: 12,
+    color: '#25396F',
+  },
+  vector: {
+    width:'100%',
+    height:339,
+  },
+
 });
