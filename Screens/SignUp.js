2af7a8936fd1982b1fe93b7b2059008b52770a62
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-07-13 01:49:39 +0430

    Update SignUp.js
    not working icons

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -14,65 +14,52 @@ export default function SignUp() {
     function press() {
         console.log('press signUp');
     }
+
+
+
     return (
 
-        <View>
+        <SafeAreaView >
+            <ScrollView >
+                <View>
 
-            <View style={{ width: '100%', height: 276, justifyContent: 'space-between', flexDirection: 'row' }}>
-                <Text style={{ fontSize: 30, lineHeight: 45, width: 143, height: 90, marginTop: 104, marginLeft: 16, color: '#3EB09B' }}>Hello Wellcome</Text>
-                <Image source={require('../assets/babySignUp.png')} resizeMode="cover" />
-            </View>
-            <View style={{ marginHorizontal: 16 }}>
+                    <View style={{ width: '100%', height: 276, justifyContent: 'space-between', flexDirection: 'row' }}>
+                        <Text style={{ fontSize: 30, lineHeight: 45, width: 143, height: 90, marginTop: 104, marginLeft: 16, color: '#3EB09B' }}>Hello Wellcome</Text>
+                        <Image source={require('../assets/babySignUp.png')} resizeMode="cover" />
+                    </View>
+                    <View style={{ marginHorizontal: 16 }}>
 
-                <View style={{ borderBottomWidth: 1, width: '98%', marginBottom: 50, }}>
+                        <View style={{ borderBottomWidth: 1, width: '98%', marginBottom: 50, }}>
 
-                    <TextInput
-                        style={{ fontSize: 16, marginHorizontal: 16, borderEndWidth: 2, color: '#3EB09B' }}
-                        onChangeText={onChangeText}
-                        value={text}
-                    />
+                            <TextInput
+                                style={{ fontSize: 16, marginHorizontal: 16, borderEndWidth: 2, color: '#3EB09B' }}
+                                onChangeText={onChangeText}
+                                value={text}
+                            />
+                        </View>
+
+
+
+                        <View style={{ borderBottomWidth: 1, width: '98%', marginBottom: 32 }}>
+
+                            <TextInput
+                                style={{ fontSize: 16, marginHorizontal: 16, borderEndWidth: 2, color: '#3EB09B' }}
+                                onChangeText={onChangeNumber}
+                                value={number}
+                                placeholder="useless placeholder"
+                                keyboardType="numeric"
+                            />
+                        </View>
+                        {/* <SignUpBottom text="signUp" color="#00866A" onPress={this.press}/> */}
+                        <SignUpBottom style={{ marginRight: 160 }} title="signUp" color="#00866A" onclick={press} />
+                        const myButton = (
+                      
+
+                    </View>
                 </View>
 
-
-
-                <View style={{ borderBottomWidth: 1, width: '98%', marginBottom: 32 }}>
-
-                    <TextInput
-                        style={{ fontSize: 16, marginHorizontal: 16, borderEndWidth: 2, color: '#3EB09B' }}
-                        onChangeText={onChangeNumber}
-                        value={number}
-                        placeholder="useless placeholder"
-                        keyboardType="numeric"
-                    />
-                </View>
-                {/* <SignUpBottom text="signUp" color="#00866A" onPress={this.press}/> */}
-                <SignUpBottom style={{ marginRight: 160 }} title="signUp" color="#00866A" onclick={press} />
-                <View style={{ flexDirection: 'row',height:300,backgroundColor:'red',width:300 }}>
-                    <Icon.Button
-                        name="facebook"
-                        backgroundColor="#3b5998"
-                        onPress={this.loginWithFacebook}
-                    >
-                        Login with Facebook
-                    </Icon.Button>
-                    <Icon.Button
-                        name="facebook"
-                        backgroundColor="#3b5998"
-                        onPress={this.loginWithFacebook}
-                    >
-                        Login with Facebook
-                    </Icon.Button>
-                    <Icon.Button
-                        name="facebook"
-                        backgroundColor="#3b5998"
-                        onPress={this.loginWithFacebook}
-                    >
-                        Login with Facebook
-                    </Icon.Button>
-                </View>
-                <View><Text>dasdakjshdjksahdkjashdkjash</Text></View>
-            </View>
-        </View>
+            </ScrollView>
+        </SafeAreaView>
 
 
 
