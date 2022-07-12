05cd04caa7703bfc4a0943a6b7ba2cb6f2bed9c8
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-07-13 00:43:44 +0430

    starting sign up

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -1,41 +1,45 @@
-import { StyleSheet, Text, View, Button, Image, ImageBackground, SafeAreaView, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Image, ImageBackground, SafeAreaView, ScrollView } from 'react-native';
+import React from 'react';
 
 export default function SignUp() {
+    const [text, onChangeText] = React.useState("Useless Text");
+    const [number, onChangeNumber] = React.useState(null);
     return (
-        <SafeAreaView>
-            <ScrollView >
-             <View style={{width:'100%',height:276,backgroundColor:'red',flexDirection:'space-between'}}>
-                <Text style={{fontSize:30,lineHeight:45,color:'#3EB09B'}}>Hello Wellcome</Text>
+
+        <View>
+
+            <View style={{ width: '100%', height: 276, justifyContent: 'space-between', flexDirection: 'row' }}>
                 <Image source={require('../assets/babySignUp.png')} resizeMode="cover" />
-             </View>
+                <Text style={{ fontSize: 30, lineHeight: 45, width: 143, height: 90, marginTop: 104, marginRight: 16, color: '#3EB09B' }}>Hello Wellcome</Text>
+            </View>
+            <View style={{ marginHorizontal: 16 }}>
+
+                <View style={{ borderBottomWidth: 1, width: '98%', marginBottom: 50, }}>
+
+                    <TextInput
+                        style={{ fontSize: 16, marginHorizontal: 16, borderEndWidth: 2,color:'#3EB09B' }}
+                        onChangeText={onChangeText}
+                        value={text}
+                    />
+                </View>
 
 
-            </ScrollView>
-        </SafeAreaView>
+
+                <View style={{ borderBottomWidth: 1, width: '98%' }}>
+
+                    <TextInput
+                        style={{ fontSize: 16, marginHorizontal: 16, borderEndWidth: 2,color:'#3EB09B' }}
+                        onChangeText={onChangeNumber}
+                        value={number}
+                        placeholder="useless placeholder"
+                        keyboardType="numeric"
+                    />
+                </View>
+            </View>
+        </View>
+
+
 
 
     );
 }
-const styles = StyleSheet.create({
-    header: {
-        flexDirection: 'row',
-        height: '100%',
-        justifyContent: 'space-between'
-    },
-    textInner: {
-        width: 43,
-        height: 90,
-        backgroundColor: 'black',
-    },
-    textHeader: {
-        color: '#3EB09B',
-        fontSize: 30,
-        lineHeight: 45,
-        marginTop: 130
-    },
-    imageCover: {
-        height: 277,
-        width: 261
-    }
-
-})
