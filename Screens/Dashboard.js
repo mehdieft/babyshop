55a6e539aff9e697158ab481425fdc5060204a09
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-07-13 12:10:48 +0430

    npm gradiant installed

--- Screens/Dashboard.js
+++ Screens/Dashboard.js
@@ -1,8 +1,30 @@
-import {View,Text} from 'react-native'
+import {View,Text,Image,ImageBackground} from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+
+
+
+
+
+
+
 export default function Dashboard() {
     return (
-        <View>
-            <Text>this is Dashboard</Text>
+        <View style={{marginVertical:16,marginHorizontal:16}}>
+            <View style={{flexDirection:'row',justifyContent: 'space-between'}}>
+                <View>
+                    <Text style={{fontSize:12,marginBottom:4,color:'#25396F'}}>Good Morning</Text>
+                    <Text style={{fontSize:24,color:'#25396F'}}>Annete</Text>
+                </View>
+                <Image source={require('../assets/icons/anne.png')} style={{borderRadius:40,width:60,height:60}} />
+
+            </View>
+            <View style={{flexDirection:'row'}}>
+                <View style={{width:160,height:151,}}>
+                    <ImageBackground ></ImageBackground>
+                </View>
+                <View></View>
+            </View>
+           
 
         </View>
     )
