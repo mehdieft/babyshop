ca5da3c5ffdbcc3a86a54a2d9ca775919c26d7d9
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-07-18 03:46:58 +0430

    ENUGH IS ENAUGH

--- Screens/WhatsUp.js
+++ Screens/WhatsUp.js
@@ -1,4 +1,5 @@
 import { View, Text, StyleSheet, Image, ImageBackground, ScrollView } from 'react-native';
+import SearchInput from '../components/SearchInput';
 
 
 export default function WhatsUp(){
@@ -6,6 +7,7 @@ export default function WhatsUp(){
         <ScrollView style={styles.container}>
         <View>
             <Text style={styles.sympthoms}>symptoms</Text>
+            <SearchInput  color="#398AA5"   placeholder="What’s your body telling you?"     />
         </View>
         </ScrollView>
     )
@@ -14,5 +16,5 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#FFFFFF', margin: 16, padding: 16, height: '50%'
     },
-    sympthoms:{marginTop:8,color:'#398AA5',fontSize:14,fontWeight:'500',lineHeight:21}
+    sympthoms:{marginTop:8,color:'#398AA5',fontSize:14,fontWeight:'500',lineHeight:21,marginBottom:20}
 })
--- components/searchInput.js
+++ components/searchInput.js
@@ -1,11 +1,16 @@
 import {View,TouchableOpacity,TextInput} from 'react-native';
+import { EvilIcons } from '@expo/vector-icons'; 
+
 import { useState } from 'react';
-export default function SearchInput({placeholder}){
+export default function SearchInput({placeholder,color}){
     const [text,changetext]=useState(null)
     
     return (
-        <View style={{flexDirection:'row'}}>
-            <TextInput onchange={onchange} placeholder={placeholder}></TextInput>
+        <View style={{flexDirection:'row',borderWidth:1,borderColor:color,height:40,alignItems:'center',justifyContent: 'space-between',paddingLeft:32,paddingRight:4,borderRadius:3}}>
+            <TextInput style={{paddingRight:13}} onChangeText={changetext} placeholder={placeholder}></TextInput>
+            <View style={{backgroundColor:color,borderRadius:5,justifyContent:'center',width:32,height:32,alignItems: 'center'}}>
+            <EvilIcons name="search" size={24} color="black" />
+            </View>
 
         </View>
 
