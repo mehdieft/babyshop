b3d080b489d0cc724e9e6789b470fc036207abca
mehdieft <50948075+mehdieft@users.noreply.github.com>
2022-07-18 15:58:37 +0430

    resuming the whats ap

--- Screens/WhatsUp.js
+++ Screens/WhatsUp.js
@@ -1,19 +1,19 @@
 import { View, Text, StyleSheet, Image, ImageBackground, ScrollView } from 'react-native';
 import SearchInput from '../components/SearchInput';
-import IconButton  from '../components/IconButton';
-import { Octicons } from '@expo/vector-icons'; 
+import IconButton from '../components/IconButton';
+import { Octicons } from '@expo/vector-icons';
 
 
 
-export default function WhatsUp(){
+export default function WhatsUp() {
     return (
         <ScrollView style={styles.container}>
-        <View>
-            <Text style={styles.sympthoms}>symptoms</Text>
-            <SearchInput  color="#398AA5"   placeholder="What’s your body telling you?"     />
-            <Text style={{marginTop: 25,marginBottom:10}}>Recent</Text>
-        </View>
-            <View style={{ flexDirection:'row',flexWrap:'wrap',margin:2,height:200}}>
+            <View>
+                <Text style={styles.sympthoms}>symptoms</Text>
+                <SearchInput color="#398AA5" placeholder="What’s your body telling you?" />
+                <Text style={{ marginTop: 25, marginBottom: 10 }}>Recent</Text>
+            </View>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', margin: 2}}>
                 <IconButton bgColor="white" color="#ACACAC" text="nothing"></IconButton>
                 <IconButton bgColor="white" color="#ACACAC" text="fatisue / exhaustion"></IconButton>
                 <IconButton bgColor="#B0D0DB" color="#ACACAC" text="dizziness">
@@ -24,9 +24,16 @@ export default function WhatsUp(){
                 </IconButton>
                 <IconButton bgColor="white" color="#ACACAC" text="shortness of breath"></IconButton>
                 <IconButton bgColor="white" color="#ACACAC" text="acen"></IconButton>
-
-                
             </View>
+            <Text style={{ marginTop: 25, marginBottom: 10 }}>Selected</Text>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', margin: 2 }}>
+                <IconButton bgColor="white" color="#ACACAC" text="nothing"></IconButton>
+                <IconButton bgColor="white" color="#ACACAC" text="fatisue / exhaustion"></IconButton>
+            </View>
+            <Text style={{ marginTop: 25, marginBottom: 10,fontSize:16,color: '#E18AB6' }}>Medications</Text>
+            <SearchInput color="#E7A1C5" placeholder="What Did You Take?" />
+
+
         </ScrollView>
     )
 }
@@ -34,5 +41,5 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#FFFFFF', margin: 16, padding: 16, height: '50%'
     },
-    sympthoms:{marginTop:8,color:'#398AA5',fontSize:14,fontWeight:'500',lineHeight:21,marginBottom:20}
+    sympthoms: { marginTop: 8, color: '#398AA5', fontSize: 14, fontWeight: '500', lineHeight: 21, marginBottom: 20 }
 })
